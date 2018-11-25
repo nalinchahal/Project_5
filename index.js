b9511d66e2109420fad58af8a387f70d7ce1176a
Nalin Chahal <nalin.chahal@gmail.com>
2018-11-25 09:09:29 -0500

    changes

--- index.js
+++ index.js
@@ -9,14 +9,3 @@ function getFormInput(){
 	console.log(searchUrl);
 	window.location.href = searchUrl;
 }
-
-input.addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === getFormInput) {
-        document.getElementById("myBtn").click();
-
-//["Oops! You broke the internet. Good Job. *SlowClap*",
-//"AT&T has denied your service due to you forget to pay your bill. Be an Adult.",
-//"Hey, you remember that song. It went like TA TA TA TA TA TA. Yeah, I miss that. Anyways your Internet Broke."
-//"There is no Google, Only Bing."
-//"Welcome To ALtavista, Your Access to 1992."]
